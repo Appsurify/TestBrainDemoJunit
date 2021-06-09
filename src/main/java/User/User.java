12d563d6ffaa1317df7fc0cd5a358ce3a763149e
appsurifysandboxuser <85136841+appsurifysandboxuser@users.noreply.github.com>
2021-06-09 15:09:05 +0200

    Update User.java
    User File modified

--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -4,7 +4,7 @@ package User;
  * Created by james on 24/05/2019.
  */
 public class User {
-    int i = 14;
+    int i = 15;
     public String dummyMethod(String input){
         return input;
     }
