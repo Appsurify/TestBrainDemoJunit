5df667bef3655f2cc72400c4441789c8e9358e48
appsurifysandboxuser <85136841+appsurifysandboxuser@users.noreply.github.com>
2021-06-28 13:08:58 +0200

    Update JointAccount.java
    change account

--- src/main/java/Accounts/JointAccount/JointAccount.java
+++ src/main/java/Accounts/JointAccount/JointAccount.java
@@ -6,7 +6,7 @@ import Accounts.*;
  * Created by james on 24/05/2019.
  */
 public class JointAccount {
-    int i = 10;
+    int i = 11;
     public String dummyMethod(String input){
         return input;
     }
