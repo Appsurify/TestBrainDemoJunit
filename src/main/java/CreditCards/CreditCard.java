7bd645cd4ec0a47b63f408e0160a3ae045582c4f
appsurifysandboxuser <85136841+appsurifysandboxuser@users.noreply.github.com>
2021-09-09 11:09:00 +0200

    Update CreditCard.java

--- src/main/java/CreditCards/CreditCard.java
+++ src/main/java/CreditCards/CreditCard.java
@@ -5,7 +5,7 @@ package CreditCards;
  */
 public class CreditCard{
     //add comment for edit
-    int i=4;
+    int i=5;
     public String dummyMethod(String input){
         return input;
     }
