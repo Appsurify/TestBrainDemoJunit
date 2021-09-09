f761ef3bce18e187710ef6c181cbff6a25bc6028
appsurifysandboxuser <85136841+appsurifysandboxuser@users.noreply.github.com>
2021-09-09 10:56:30 +0200

    Update Accounts.java

--- src/main/java/Accounts/Accounts.java
+++ src/main/java/Accounts/Accounts.java
@@ -4,7 +4,7 @@ package Accounts;
  * Created by james on 24/05/2019.
  */
 public class Accounts {
-    int i = 1;
+    int i = 2;
     public String dummyMethod(String input){
         return input;
     }
